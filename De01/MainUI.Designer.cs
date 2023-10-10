655a5edb52c457d71ad28261a4ae7f731635e52c
Kagamine Haku <blomaway@gmail.com>
2023-10-10 19:21:16 +0700

    Add project files.

---
+++ De01/MainUI.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace De01
+{
+    partial class MainUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainUI));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.DGV1 = new System.Windows.Forms.DataGridView();
+            this.masv = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hovaten = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bday = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lop = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "THÔNG TIN CHI TIẾT";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 87);
+            this.textBox1.MaxLength = 6;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(107, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(269, 87);
+            this.textBox2.MaxLength = 40;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(256, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(123, 226);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(402, 20);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // DGV1
+            // 
+            this.DGV1.AllowUserToAddRows = false;
+            this.DGV1.AllowUserToDeleteRows = false;
+            this.DGV1.AllowUserToResizeColumns = false;
+            this.DGV1.AllowUserToResizeRows = false;
+            this.DGV1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DGV1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.masv,
+            this.hovaten,
+            this.bday,
+            this.lop});
+            this.DGV1.Location = new System.Drawing.Point(31, 261);
+            this.DGV1.Name = "DGV1";
+            this.DGV1.Size = new System.Drawing.Size(494, 146);
+            this.DGV1.TabIndex = 5;
+            // 
+            // masv
+            // 
+            this.masv.HeaderText = "Mã SV";
+            this.masv.Name = "masv";
+            this.masv.ReadOnly = true;
+            // 
+            // hovaten
+            // 
+            this.hovaten.HeaderText = "Họ và tên";
+            this.hovaten.Name = "hovaten";
+            this.hovaten.ReadOnly = true;
+            // 
+            // bday
+            // 
+            this.bday.HeaderText = "Ngày sinh";
+            this.bday.Name = "bday";
+            this.bday.ReadOnly = true;
+            // 
+            // lop
+            // 
+            this.lop.HeaderText = "Lớp";
+            this.lop.Name = "lop";
+            this.lop.ReadOnly = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(123, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Xoá";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(207, 178);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Sửa";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(288, 178);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Lưu";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(369, 178);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Không Lưu";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(31, 224);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Tìm kiếm";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Mã sinh viên:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(214, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Họ Tên:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(214, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Lớp học:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Ngày sinh";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(269, 139);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(256, 21);
+            this.comboBox1.TabIndex = 16;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(450, 178);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 17;
+            this.button7.Text = "Thoát";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(101, 139);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(107, 20);
+            this.dateTimePicker1.TabIndex = 18;
+            // 
+            // MainUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DGV1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainUI";
+            this.Text = "Quan Ly Sinh Vien";
+            ((System.ComponentModel.ISupportInitialize)(this.DGV1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataGridView DGV1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn masv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hovaten;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bday;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lop;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ De01/MainUI.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace De01
+{
+    public partial class MainUI : Form
+    {
+        public MainUI()
+        {
+            InitializeComponent();
+            LoadClassNames();
+            InitDatatoDGV();
+            InitComp();
+        }
+
+        private string connectstring = "Data Source=.;Initial Catalog=QuanlySV;Integrated Security=True;MultipleActiveResultSets=True";
+        private void button7_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có muốn thoát chương trình không?", "Xác nhận thoát", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+
+        private void ClearTextBox()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void InitComp()
+        {
+            button4.Enabled=false;
+            button5.Enabled = false;
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void SaveNotSave()
+        {
+            button4.Enabled = true;
+            button5.Enabled = true;
+        }
+
+        private void LoadClassNames()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectstring))
+                {
+                    connection.Open();
+
+                    string query = "SELECT TenLop FROM Lop";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string tenLop = reader["TenLop"].ToString();
+                                comboBox1.Items.Add(tenLop);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+
+        private void InitDatatoDGV()
+        {
+            DGV1.Rows.Clear();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectstring))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM Sinhvien";
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        DGV1.Rows.Add(row["MaSV"], row["HotenSV"], row["BDay"], row["MaLop"]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin");
+                return;
+            }
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectstring))
+                {
+                    connection.Open();
+
+                    string queryLop = "SELECT MaLop FROM Lop WHERE TenLop = @TenLop";
+                    using (SqlCommand commandLop = new SqlCommand(queryLop, connection))
+                    {
+                        commandLop.Parameters.AddWithValue("@TenLop", comboBox1.SelectedItem.ToString());
+                        string maLop = commandLop.ExecuteScalar() as string;
+
+                        if (!string.IsNullOrEmpty(maLop))
+                        {
+                            string query = "INSERT INTO Sinhvien (MaSV, HotenSV, BDay, MaLop) VALUES (@MaSV, @HotenSV, @BDay, @MaLop)";
+
+                            using (SqlCommand command = new SqlCommand(query, connection))
+                            {
+                                command.Parameters.AddWithValue("@MaSV", textBox1.Text);
+                                command.Parameters.AddWithValue("@HotenSV", textBox2.Text);
+                                command.Parameters.AddWithValue("@BDay", dateTimePicker1.Value);
+                                command.Parameters.AddWithValue("@MaLop", maLop);
+
+                                command.ExecuteNonQuery();
+
+                                MessageBox.Show("Thêm dữ liệu thành công!");
+                                InitDatatoDGV();
+                                ClearTextBox();
+
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Không tìm thấy mã lớp tương ứng!");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            string searchText = textBox4.Text.Trim();
+            foreach (DataGridViewRow row in DGV1.Rows)
+            {
+                string studentName = row.Cells["hovaten"].Value.ToString();
+                if (studentName.IndexOf(searchText, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    row.Visible = true;
+                }
+                else
+                {
+                    row.Visible = false;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (DGV1.SelectedRows.Count > 0)
+                {
+                    DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn xoá sinh viên này không?", "Xác nhận xoá", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        string maSV = DGV1.SelectedRows[0].Cells["masv"].Value.ToString();
+
+                        using (SqlConnection connection = new SqlConnection(connectstring))
+                        {
+                            connection.Open();
+
+                            string query = "DELETE FROM Sinhvien WHERE MaSV = @MaSV";
+
+                            using (SqlCommand command = new SqlCommand(query, connection))
+                            {
+                                command.Parameters.AddWithValue("@MaSV", maSV);
+                                command.ExecuteNonQuery();
+
+                                DGV1.Rows.RemoveAt(DGV1.SelectedRows[0].Index);
+
+                                MessageBox.Show("Xoá sinh viên thành công!");
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Chưa chọn sinh viên để xoá.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+
+        private void AddDelButtonEnable()
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+
+        private void AddDelButtonDisable()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (DGV1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = DGV1.SelectedRows[0];
+                string maSV = selectedRow.Cells["masv"].Value.ToString();
+                string hoTen = selectedRow.Cells["hovaten"].Value.ToString();
+                DateTime ngaySinh = DateTime.Parse(selectedRow.Cells["BDay"].Value.ToString());
+                string lopHoc = selectedRow.Cells["lop"].Value.ToString();
+                textBox1.Text = maSV;
+                textBox2.Text = hoTen;
+                dateTimePicker1.Value = ngaySinh;
+                AddDelButtonDisable();
+                textBox1.Enabled= false;
+                SaveNotSave();
+            }
+            else
+            {
+                MessageBox.Show("Chưa chọn sinh viên để hiển thị thông tin.");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string maSV = textBox1.Text;
+
+
+                if (string.IsNullOrEmpty(maSV))
+                {
+                    MessageBox.Show("Vui lòng nhập mã số sinh viên.");
+                    return;
+                }
+
+                if (DeleteStudentByMaSV(maSV))
+                {
+
+                    AddStudent();
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy sinh viên có mã số: " + maSV);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+            AddDelButtonEnable();
+            textBox1.Enabled = true;
+            InitComp();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            AddDelButtonEnable();
+            textBox1.Text=null; 
+            textBox2.Text= null;
+            textBox1.Enabled = true;
+            InitComp();
+            InitDatatoDGV();
+        }
+
+        private bool DeleteStudentByMaSV(string maSV)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectstring))
+                {
+                    connection.Open();
+
+                    string deleteQuery = "DELETE FROM Sinhvien WHERE MaSV = @MaSV";
+
+                    using (SqlCommand deleteCommand = new SqlCommand(deleteQuery, connection))
+                    {
+                        deleteCommand.Parameters.AddWithValue("@MaSV", maSV);
+                        deleteCommand.ExecuteNonQuery();
+                        return true;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false; 
+            }
+        }
+
+        private void AddStudent()
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin.");
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectstring))
+                {
+                    connection.Open();
+
+                    string queryLop = "SELECT MaLop FROM Lop WHERE TenLop = @TenLop";
+                    using (SqlCommand commandLop = new SqlCommand(queryLop, connection))
+                    {
+                        commandLop.Parameters.AddWithValue("@TenLop", comboBox1.SelectedItem.ToString());
+                        string maLop = commandLop.ExecuteScalar() as string;
+
+                        if (!string.IsNullOrEmpty(maLop))
+                        {
+                            string query = "INSERT INTO Sinhvien (MaSV, HotenSV, BDay, MaLop) VALUES (@MaSV, @HotenSV, @BDay, @MaLop)";
+
+                            using (SqlCommand command = new SqlCommand(query, connection))
+                            {
+                                command.Parameters.AddWithValue("@MaSV", textBox1.Text);
+                                command.Parameters.AddWithValue("@HotenSV", textBox2.Text);
+                                command.Parameters.AddWithValue("@BDay", dateTimePicker1.Value);
+                                command.Parameters.AddWithValue("@MaLop", maLop);
+
+                                command.ExecuteNonQuery();
+
+                                MessageBox.Show("Cập nhật thông tin sinh viên thành công!");
+                                InitDatatoDGV();
+                                ClearTextBox();
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Không tìm thấy mã lớp tương ứng!");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
